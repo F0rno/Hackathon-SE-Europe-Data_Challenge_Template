2ca8c40fd16a0674e24d856575052dc0085ce6d9
F0rno <pablo.fornell.perinan@gmail.com>
2023-11-19 17:44:18 +0100

    load_data, now add country column to check origin of the information later

--- src/data_processing.py
+++ src/data_processing.py
@@ -3,6 +3,8 @@ import pandas as pd
 from os import listdir, remove
 from os.path import isfile
 
+country_initials = ['SP', 'UK', 'DE', 'DK', 'HU', 'SE', 'IT', 'PO', 'NL']
+
 def delete_fossil_data():
     green_energy_codes = ['B09', 'B10', 'B11', 'B12', 'B13', 'B14', 'B15', 'B16', 'B18', 'B19']
     # List cvs files of .data/
@@ -25,7 +27,15 @@ def load_data(data_path):
             continue
         if 'test' in file_path.split('.'):
             continue
-        dfs.append(pd.read_csv(data_path + file_path))
+        df = pd.read_csv(data_path + file_path)
+        file_path_splited = file_path.split('_')
+        # Adding the country initials in generation_data
+        if file_path_splited[1] in country_initials:
+            df['country'] = file_path_splited[1]
+        # Adding the country initials in load_data
+        if file_path_splited[-1].replace('.csv', '') in country_initials:
+            df['country'] = file_path_splited[-1]
+        dfs.append(df)
     return dfs
 
 def clean_data(dfs):
@@ -35,9 +45,25 @@ def clean_data(dfs):
         dfs.dropna()
     return dfs
 
-def preprocess_data(df):
+def preprocess_data(dfs):
+    #{
+    #    SP: 0, # Spain
+    #    UK: 1, # United Kingdom
+    #    DE: 2, # Germany
+    #    DK: 3, # Denmark
+    #    HU: 5, # Hungary
+    #    SE: 4, # Sweden
+    #    IT: 6, # Italy
+    #    PO: 7, # Poland
+    #    NL: 8 # Netherlands
+    #}
     # TODO: Generate new features, transform existing features, resampling, etc.
+
+    # Search for the country code in the file name to get the country data: gen and load
     
+
+    # Acumulate data of the country like (quantity), (load) by time lapse hour
+    # Add the data country to a all_data.cvs file
     return df_processed
 
 def save_data(df, output_file):
@@ -67,7 +93,7 @@ def main(input_file, output_file):
     save_data(df_processed, output_file)
 
 if __name__ == "__main__":
-    clean_data(load_data('./data/'))
+    load_data('./data/')
     exit(0)
     args = parse_arguments()
     main(args.input_file, args.output_file)
